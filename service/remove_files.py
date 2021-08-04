9c7df72c31e30f229c9045cb3a1f1559d84f7e58
Fredrik Carlsson <me@fc.io>
2021-08-04 11:33:20 +0200

    reactor function name
    make it a bit more clear that it's only temporarily generated files
    that are removed

--- service/remove_files.py
+++ service/remove_files.py
@@ -5,7 +5,7 @@ import aiofiles.os
 
 from service.utils.get_tmp_data_path import get_tmp_data_path
 
-def exist_in_folder(file, folder_path):
+def exist_in_tmp_folder(file, folder_path):
     files_in_folder_path = listdir(folder_path)
 
     return any((file == join(folder_path, i) for i in files_in_folder_path))
@@ -14,5 +14,5 @@ async def remove_files (files, path_to_svg):
     files['svg'] = path_to_svg
 
     for file in files.values():
-        if isfile(file) and exist_in_folder(file, get_tmp_data_path()):
+        if isfile(file) and exist_in_tmp_folder(file, get_tmp_data_path()):
             await aiofiles.os.remove(file)
